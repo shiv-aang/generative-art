40ec41cacde41b1b73249f5b5922f618d66418e3
shivaang <shivaangupadhyay@gmail.com>
2021-09-01 05:33:17 +0530

    p5js code

--- Broken_interest_2021_08_31_22_33_50/sketch.js
+++ Broken_interest_2021_08_31_22_33_50/sketch.js
@@ -1,33 +1,33 @@
 let img;
 let cnv;
 function preload(){
-  img = loadImage('assets/walk.jpg');
+  img = loadImage('assets/window.jpg');
 }
 
 function setup() {
-  cnv= createCanvas(img.width, img.height);
-  let newCanvasX= (windowWidth - img.width)/2;
-  let newCanvasY=(windowWidth -img.width)/2;
+  cnv= createCanvas(1000, 900);
+  let newCanvasX= (windowWidth - img.width)/4;
+  let newCanvasY=(windowWidth -img.width)/4;
   cnv.position(newCanvasX, newCanvasY);
-  for(let col= 0; col<img.width; col+=10){
-    for(let row=0;row<img.height; row+=10){
+  for(let col= 0; col<img.width; col+=random(10)){
+    for(let row=0;row<img.height; row+=random(5)){
       let xPos= col;
       let yPos= row;
-      let zPos= col;
+      let zPos= row;
       let c = img.get(xPos, yPos,);
       push();
       translate(xPos, yPos, zPos);
       rotate(radians(random(120)))
       //noFill();
       fill(color(c));
-      stroke(0);
+      stroke('white');
       //stroke(color(c));
-      strokeWeight(random(15))
-      point(xPos, yPos,zPos);
+      strokeWeight(random(30))
+      point(xPos*random(10), yPos,zPos*random(15));
       strokeWeight(random(5));
       //quad(x1, y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4, [detailX], [detailY])
 
-      quad(xPos, yPos, zPos, xPos*random(5), yPos*random(2), zPos*random(3), yPos*random(5), yPos*random(4), zPos*random(5), xPos*random(2), yPos*random(3), xPos*random(4));
+      quad(xPos, yPos, zPos, xPos*random(5), zPos*random(2), zPos*random(3), xPos*random(5), yPos*random(4), zPos*random(5), xPos*random(2), yPos*random(3), xPos*random(4));
       //triangle(x1, y1, x2, y2, x3, y3)
       //triangle(xPos,yPos, xPos*random(5), yPos*random(3), xPos*random(3), yPos*random(10));
       
